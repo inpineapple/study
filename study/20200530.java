0667a8311950cc24588a65e1107c345bcf5b3397
inpineapple <appleterminal@naver.com>
2020-05-31 17:04:21 +0900

    2446별찌기

---
+++ study/20200530.java
@@ -0,0 +1,28 @@
+//2523별찍기
+
+import java.util.Scanner;
+
+public class Main6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=i+1; j<n; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ study/20200531.java
@@ -0,0 +1,33 @@
+//2446 별찍기
+
+import java.util.Scanner;
+
+public class Main7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=2*i-1; j<2*(n-1); j++)
+					System.out.print("*");
+			System.out.println();
+		}
+		for(int i=n; i>1; i--) {
+			for(int j=1; j<i-1;j++) {
+				System.out.print(" ");
+			}
+			for(int j=2*(n+2) ;j>2*i+1; j--)
+					System.out.print("*");
+			System.out.println();
+		}
+		
+		
+	}
+
+}
