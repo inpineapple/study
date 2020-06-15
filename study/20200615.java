e3650fc4efd54fcfa1e371eda939154de5fc3945
inpineapple <48673059+inpineapple@users.noreply.github.com>
2020-06-15 21:27:16 +0900

    1152

---
+++ study/20200615.java
@@ -0,0 +1,22 @@
+//1152
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+
+public class Main15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		String line=sc.nextLine();
+		StringTokenizer st=new StringTokenizer(line);
+		System.out.println(st.countTokens());
+		
+		sc.close();
+		
+	}
+
+}
