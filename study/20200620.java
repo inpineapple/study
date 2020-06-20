a881afeb7713312c62fe701033d25e8e0d09b364
inpineapple <48673059+inpineapple@users.noreply.github.com>
2020-06-20 16:36:30 +0900

    11720

---
+++ study/20200620.java
@@ -0,0 +1,22 @@
+//11720
+import java.util.Scanner;
+
+
+public class Main16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String str=sc.next();
+		int sum=0;
+		for(int i=0; i<n; i++) {
+			sum+=Integer.parseInt(""+str.charAt(i));
+		}
+		System.out.println(sum);
+		sc.close();
+		
+	}
+
+}
